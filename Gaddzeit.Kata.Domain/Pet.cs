d303b87bd8d56169d3ac7beba1a1ee41f5bcd00f
David Gadd <gaddzeit@yahoo.ca>
2010-05-13 20:45:45 -0700

    Added details to the README file.

--- Gaddzeit.Kata.Domain/Pet.cs
+++ Gaddzeit.Kata.Domain/Pet.cs
@@ -1,6 +1,20 @@
-﻿namespace Gaddzeit.Kata.Domain
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gaddzeit.Kata.Domain
 {
     public class Pet : DomainEntity
     {
+        public List<Treatment> Treatments { get; private set; }
+
+        public Pet()
+        {
+            this.Treatments = new List<Treatment>();
+        }
+
+        public void AddTreatment(Treatment treatment)
+        {
+            this.Treatments.Add(treatment);
+        }
     }
 }
--- Gaddzeit.Kata.Tests.Unit/PetTests.cs
+++ Gaddzeit.Kata.Tests.Unit/PetTests.cs
@@ -13,5 +13,22 @@ namespace Gaddzeit.Kata.Tests.Unit
             var sut = new Pet();
             sut.ShouldBeInstanceOf<DomainEntity>();
         }
+
+        [Test]
+        public void TreatmentsProperty_Get_HasCountOf0Initially()
+        {
+            var sut = new Pet();
+            sut.Treatments.Count.ShouldEqual(0);
+        }
+
+        [Test]
+        public void AddTreatmentMethod_PetInputParam_IncremantsTreatmentsCount()
+        {
+            var sut = new Pet();
+            sut.Treatments.Count.ShouldEqual(0);
+            sut.AddTreatment(new Treatment(Treatment.TreatmentEnum.Hydration));
+            sut.Treatments.Count.ShouldEqual(1);
+        }
+
     }
 }
